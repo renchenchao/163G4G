4664b6820dbbcebcd5f940c43d483977b57a1458
renchenchao <1209423816@qq.com>
2017-12-03 15:38:10 +0800

    任臣朝提交struts2/T6上机作业

---
+++ Struts2/T6/07任臣朝/Struts2_T6/src/com/rcc/t6/action/LocalAction.java
@@ -0,0 +1,15 @@
+package com.rcc.t6.action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LocalAction extends ActionSupport {
+
+	/* (non-Javadoc)
+	 * @see com.opensymphony.xwork2.ActionSupport#execute()
+	 */
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+}
